6af3a9b0d327d612c5b115809ee0d76b4d459d8a
Recai-Capkin <recai.capkin28@gmail.com>
2024-04-26 14:54:27 +0300

    updated repo

---
+++ Tutorial_1/Variables_Constants/target/debug/deps/Variables_Constants-2fb6d11adda60496.d
@@ -0,0 +1,5 @@
+C:\Users\Recai\RustStaff\Tutorial_1\Variables_Constants\target\debug\deps\libVariables_Constants-2fb6d11adda60496.rmeta: src\main.rs
+
+C:\Users\Recai\RustStaff\Tutorial_1\Variables_Constants\target\debug\deps\Variables_Constants-2fb6d11adda60496.d: src\main.rs
+
+src\main.rs:
---
+++ Tutorial_1/Variables_Constants/target/debug/deps/Variables_Constants-4ab493855ac93c54.d
@@ -0,0 +1,5 @@
+C:\Users\Recai\RustStaff\Tutorial_1\Variables_Constants\target\debug\deps\libVariables_Constants-4ab493855ac93c54.rmeta: src\main.rs
+
+C:\Users\Recai\RustStaff\Tutorial_1\Variables_Constants\target\debug\deps\Variables_Constants-4ab493855ac93c54.d: src\main.rs
+
+src\main.rs:
